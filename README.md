29ac2f364a67324b006f12f9e2d457a75f443e01
Abhishek Singh <abhishek.dumca@gmail.com>
2025-02-22 19:36:34 +0530

    added alembic for data migration

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # TodoApp
+
+## Alembic
+
+```bash
+alembic init alembic
+```
+
+## Alembic create revision
+
+```bash
+alembic revision -m "Create phone number for user column"
+```
+
+## Alembic upgrade
+
+```bash
+alemibic upgrade <revision-id>
+```
+
+## Alembic downgrade
+
+```bash
+alembic downgrade -1
+```
--- database.py
+++ database.py
@@ -2,15 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@localhost/todo_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://admin:db123@localhost/todo_db"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 #
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# connect_args={"check_same_thread": False} is used for SQLite databases, to allow the connection to be used by multiple threads.
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
